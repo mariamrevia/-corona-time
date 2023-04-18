65de61ea303876f1d0eb45ccf242e6a1ad482f04
mariamrevia <mariam.revia@redberry.ge>
2023-04-18 21:37:38 +0400

    refactor:route name

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'login')->name('login');
+Route::view('/', 'login')->name('login.show');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
-Route::view('register', 'register')->name('register');
+Route::view('register', 'register')->name('register.show');
 Route::post('register', [RegisterController::class, 'store'])->name('register.store');
 Route::view('confirm', 'email.confirm')->name('email.confirm');
