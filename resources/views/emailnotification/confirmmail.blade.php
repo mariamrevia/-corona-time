03b02dd73edb0edfe3943b36d4366afcae6c1d9d
mariamrevia <mariam.revia@redberry.ge>
2023-04-20 14:43:10 +0400

    style:confirm notification

--- resources/views/emailnotification/confirmmail.blade.php
+++ resources/views/emailnotification/confirmmail.blade.php
@@ -1,18 +1,75 @@
-<x-mail::message> 
-    @component('mail::message')
-    ![Image description](https://i.ibb.co/prc027h/Landing.png)
-    <div style="background-color: #FFFFFF; padding: 24px">
-        <h1 class="text-2xl font-bold text-gray-800 mb-2">Confirmation email</h1>
-        <p class="text-gray-700">click this button to verify your email</p>
-    </div>
-    @component('mail::button', [
-        'url' => $verificationUrl,
-        'color' => 'green',
-        'class' => 'w-full mt-6',
-    ])
-        Verify Email
-    @endcomponent
+<body
+    style=" position: relative; -webkit-text-size-adjust: none; height: 100vh; line-height: 1.4; margin: 0; padding: 0; width: 100% !important; background-color: #ffffff;">
 
-    Thanks,<br>
-    {{ config('app.name') }}
-</x-mail::message>
+    <table class="wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation"
+        style=" position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;  margin: 0; padding: 0; width: 100%;">
+        <tr>
+            <td align="center" style=" position: relative;">
+                <table class="content" width="100%" cellpadding="0" cellspacing="0" role="presentation"
+                    style=" position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; margin: 0; padding: 0; width: 100%;">
+                 
+                    <tr>
+                        <td class="body" width="100%" cellpadding="0" cellspacing="0"
+                            style=" position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;  border-bottom: 1px solid #edf2f7; border-top: 1px solid #edf2f7; margin: 0; padding: 0; width: 100%; border: hidden !important;">
+                            <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0"
+                                role="presentation"
+                                style=" position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px; background-color: #ffffff; border-color: #e8e5ef; border-radius: 2px; border-width: 1px; box-shadow: 0 2px; margin: 0 auto; padding: 0; width: 570px;">
+                              
+                                <tr>
+                                    <td class="content-cell" align="center"
+                                        style=" position: relative; max-width: 100vw; padding: 32px; ">
+                                        <img style="position: relative;display: inline-block;  text-align: center; margin-bottom: 56px;"
+                                            src="https://i.ibb.co/t4v26cR/Landing-Worldwide-2.png" />
+                                        <h1
+                                            style="margin-bottom: 16px; position: relative; color: #010414; font-size: 25px; font-weight: bold; margin-top: 0; text-align: center;">
+                                            Confirmation email</h1>
+                                        <p
+                                            style="margin-bottom: 40px; position: relative; color: #010414 font-size: 18px; margin-top: 0; text-align: center;">
+                                            click this button to verify your email</p>
+                                        <table class="action" align="center" width="100%" cellpadding="0"
+                                            cellspacing="0" role="presentation"
+                                            style=" position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; margin: 30px auto; padding: 0; text-align: center; width: 100%;">
+                                            <tr>
+                                                <td align="center" style=" position: relative;">
+                                                    <table width="100%" border="0" cellpadding="0" cellspacing="0"
+                                                        role="presentation" style=" position: relative;">
+                                                        <tr>
+                                                            <td align="center" style=" position: relative;">
+                                                                <table border="0" cellpadding="0" cellspacing="0"
+                                                                    role="presentation" style=" position: relative;">
+                                                                    <tr>
+                                                                        <td style=" position: relative;">
+                                                                            <a href="{{ $verificationUrl }}"
+                                                                                class="button button-primary"
+                                                                                target="_blank" rel="noopener"
+                                                                                style=" position: relative;
+                                                                                 -webkit-text-size-adjust: none; 
+                                                                                 border-radius: 8px; color: #fff; display: inline-block;
+                                                                                  overflow: hidden; text-decoration: none; background-color: #0FBA68; 
+                                                                                  border-bottom: 19px solid #0FBA68;
+                                                                                   border-left: 137px solid #0FBA68;
+                                                                                    border-right: 137px solid #0FBA68;
+                                                                                     border-top: 19px solid #0FBA68;
+                                                                                     font-weight:bold;
+                                                                                     ">VERIFY
+                                                                                EMAIL
+                                                                            </a>
+                                                                        </td>
+                                                                    </tr>
+                                                                </table>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
