0abdba453a490875fa7b358249930a89a2e9c861
mariamrevia <mariam.revia@redberry.ge>
2023-04-29 22:46:27 +0400

    test: artisan command FetchData

---
+++ tests/Feature/FetchDataTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class FetchDataTest extends TestCase
+{
+	/**
+	 * A basic feature test example.
+	 */
+	use RefreshDatabase;
+
+	public function test_it_fetches_and_saves_data_successfully()
+	{
+		$this->artisan('app:fetch-data')->expectsOutput('Data fetched and saved successfully.');
+
+		$this->assertDatabaseCount('covid_statistics', 105);
+	}
+}
