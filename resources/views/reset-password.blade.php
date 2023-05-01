89b1e1e25e913b7f5d4267ed2cfdfc520b429ab6
mariamrevia <mariam.revia@redberry.ge>
2023-05-01 18:22:44 +0400

    style:fix input focus

--- resources/views/reset-password.blade.php
+++ resources/views/reset-password.blade.php
@@ -8,15 +8,24 @@
         
             <input type="hidden" name="token" value="{{$request->route('token') }}" >
             <input type="hidden" name="email"  value="{{$_REQUEST['email']}}">
-            
+            @php
+            $errorClass = $errors->has('password') ? 'border-red-500' : '';
+            $validClass = $errors->count() == 0 && !old('password') ? '' : ($errors->has('password') ? '' : 'border-green-500');
+        @endphp
             <div>
                 <x-input name="password" type="password" :text="__('passreset.New_Pass')"/>
                 <div class="flex flex-col mobile:mt-[1.5rem] mt-[1rem]">
-                    <label class="font-bold"  for='password'>{{__('passreset.Repeat_Pass')}}</label>
-                    <input
-                    class="w-[21.4rem] h-[3.5rem] border rounded mt-[0.5rem]" 
-                    name='password_confirmation'
-                    type="password" />
+                    <label class="font-bold" for='password'>{{ __('passreset.Repeat_Pass') }}</label>
+                    <div class="w-[21.4rem] h-[3.5rem] flex items-center border-[2px] rounded mt-[0.5rem] focus-within:border-primaryBlue  focus-within:ring focus:shadow-shadow {{ $errorClass }} {{ $validClass }}">
+                        <div class=" w-[21.4rem] flex flex-row items-center justify-center">
+                    <input placeholder="{{ __('register.RepatPass_Ph') }}"
+                        class="w-[17rem] h-[3rem] outline-none"
+                        name='password_confirmation' type="password" />
+                        @if ($validClass)   
+                        <img src="{{ asset('images/Vector1.png') }}" class="w-[20px] h-[20px]"  />
+                        @endif
+                    </div>
+                </div>
                 </div>
                 <x-error name="password" />
 
